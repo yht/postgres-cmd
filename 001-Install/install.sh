a4ff4f5ce7a1c9438d449e6756dc0a40d4d84c1e
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2014-10-17 21:18:34 +0700

    Skrip instalasi (debian)

---
+++ 001-Install/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Skrip ini sebenarnya gak perlu
+
+# cek root
+if [[ $EUID ne 0 ]; then
+	echo "Maaf, Anda tidak berhak melakukan pemasangan" 1>&2
+	exit 1
+fi
+
+# cek apt-get
+if [ -x /usr/bin/apt-get ]; then
+	apt-get update
+	apt-get install postgresql
+else
+	echo "Maaf, skrip ini hanya untuk OS linux dengan pemaketan Debian" 1>&2
+fi
